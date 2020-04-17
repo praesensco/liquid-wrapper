ca35b5ac5eec66ded1598176524a31d080149d02
widmanski <adamwidmanski@gmail.com>
2020-04-17 18:16:10 +0200

    Create LiquidWrapper component

---
+++ index.js
@@ -0,0 +1,71 @@
+import Parser from 'html-react-parser';
+import PropTypes from 'prop-types';
+import React, { Component, useState, useEffect } from 'react';
+import { Liquid } from 'liquidjs';
+
+let engine;
+
+const getEngine = (root = '/snippets') => {
+  if (!engine) {
+    engine = new Liquid({
+      extname: '.liquid',
+      root,
+    });
+  }
+
+  return engine;
+};
+
+class LiquidWrapper extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      html: '',
+    };
+  }
+
+  async renderHtml() {
+    const { template, fixture } = this.props;
+    try {
+      const engine = getEngine();
+      const html = await engine.parseAndRender(
+        template,
+        fixture,
+      );
+      this.setState({ html });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async componentDidMount() {
+    this.renderHtml();
+  }
+
+  async componentDidUpdate(prevProps, prevState) {
+    if (JSON.stringify(prevProps.fixture) !== JSON.stringify(this.props.fixture)) {
+      this.renderHtml();
+    }
+    if (prevState.html !== this.state.html) {
+      this.props.init();
+    }
+  }
+
+  render() {
+    return Parser(this.state.html);
+  }
+}
+
+LiquidWrapper.propTypes = {
+  fixture: PropTypes.object,
+  init: PropTypes.func,
+  template: PropTypes.string.isRequired,
+};
+
+LiquidWrapper.defaultProps = {
+  fixture: {},
+  init: () => {},
+};
+
+export default LiquidWrapper;
